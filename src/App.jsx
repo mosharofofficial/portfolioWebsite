cf7837402705beeaf9412147c2add9cc3986f0bd
rasc3ta <greenfair.shan5@gmail.com>
2024-10-12 03:49:09 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <div className="text-white bg-black">
+      <div className="min-h-[50vh] p-5 flex items-center justify-center ">
+        <div className=" flex flex-col justify-start items-start">
+          <img src=""  />
+          <h1 className="text-7xl font-anton px-3 pb-2 mb-2 border-b-2">
+            <span className="drop-shadow-[6px_4px_0px_#ffd700] hover:text-accent hover:drop-shadow-[6px_4px_0px_#fff] transition-all hover:transition-all duration-[300ms] hover:duration-[300ms]">
+              MOSHAROF HOSSAIN
+            </span>
+          </h1>
+          <h2 className="text-4xl pl-4">Web developer</h2>
+          <p className="max-w-[550px] pl-4 mt-6">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis esse, tempore dolore inventore eos delectus ipsum eum, sit voluptatibus voluptates animi expedita assumenda, id culpa ex nesciunt officia minima quasi!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
