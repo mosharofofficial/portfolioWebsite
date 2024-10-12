75ee12ecafd5da2af770bbef81893ec3f1272a44
rasc3ta <greenfair.shan5@gmail.com>
2024-10-12 18:34:13 +0600

    added about me section

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,30 @@ import Projects from "./components/Projects";
 
 function App() {
   return (
-    <div className="text-white bg-black min-h-screen 
-    ">
-      <div className="w-90vw max-w-[1280px] mx-auto">
-
-      <Header></Header>
-      <Skills></Skills>
-      <Projects></Projects>
+    <div
+      className="text-white bg-black min-h-screen 
+    "
+    >
+      <div className="w-90vw max-w-[1280px] mx-auto ">
+        <Header></Header>
+        <div className=" my-[100px] flex flex-col items-center justify-center p-10 border-t-[4px] border-white">
+          <h2 className="text-3xl border-b-2 border-white pb-4 mb-[40px] px-10 ">
+            About me
+          </h2>
+          <p className="text-xl max-w-[1024px] ">
+            I am a passionate junior web developer with expertise in HTML, CSS,
+            JavaScript, and React. With hands-on experience in both front-end
+            and back-end technologies like Express.js, MongoDB, and Firebase, I
+            enjoy building responsive, user-friendly web applications. I am
+            constantly learning new tools and improving my skills to create
+            innovative solutions. In addition to web development, I have
+            contributed to various college projects and volunteered to support
+            student activities. My goal is to continue growing as a full-stack
+            developer while contributing to meaningful projects.
+          </p>
+        </div>
+        <Skills></Skills>
+        <Projects></Projects>
       </div>
     </div>
   );
