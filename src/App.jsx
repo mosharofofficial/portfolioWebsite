d80cf6649c5ec3497900b93bc411d7ae3fae93f6
rasc3ta <greenfair.shan5@gmail.com>
2024-10-12 16:15:08 +0600

    added projects section

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@
 
 import Header from "./components/Header";
 import Skills from "./components/Skills";
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <div className="text-white bg-black min-h-screen 
     ">
-      <div className="w-90vw max-w-[1280px]">
+      <div className="w-90vw max-w-[1280px] mx-auto">
 
       <Header></Header>
       <Skills></Skills>
+      <Projects></Projects>
       </div>
     </div>
   );
---
+++ src/components/Projects.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <div className="flex flex-col items-center justify-center p-4 mb-[100px]">
+      <h2 className="text-3xl border-b-2 border-white pb-4 mb-[40px] px-10 ">
+        Projects{" "}
+      </h2>
+      <div className="grid">
+        <div className="flex flex-col min-w-[390px] border-2 rounded-md ">
+          <h3 className="text-2xl p-2 mb-2 border-b-[2px]">
+            Name: BloodSpring
+          </h3>
+          <h3 className="text-2xl p-2 mb-2 border-b-[2px] ">
+            Live site:&nbsp;
+            <a
+              className="underline"
+              target="_blank"
+              href="https://bloodspring-7db88.web.app/"
+            >
+              https://bloodspring-7db88.web.app/
+            </a>
+          </h3>
+          <h3 className="text-lg p-2 mb-2 ">
+            Github(Front-End):&nbsp;
+            <a
+              className="underline"
+              target="_blank"
+              href="https://github.com/Rasc3ta/BloodSpring"
+            >
+              https://github.com/Rasc3ta/BloodSpring
+            </a>
+          </h3>
+          <h3 className="text-lg p-2 mb-2 border-b-[2px]">
+            Github(Front-End):&nbsp;
+            <a
+              className="underline"
+              target="_blank"
+              href="https://github.com/Rasc3ta/BloodSpring"
+            >
+              https://github.com/Rasc3ta/BloodSpring
+            </a>
+          </h3>
+          <p className="p-2">
+            <span className="text-2xl ">Description: </span>
+            Bloodspring is a blood donation platform where users create
+            accounts, post requests, and connect with donors who can contact
+            them directly to provide help.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
