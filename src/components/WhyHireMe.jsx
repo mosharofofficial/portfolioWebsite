2c65d0d7c1b56a99aec4759ab3795e97fd7f1170
rasc3ta <greenfair.shan5@gmail.com>
2024-10-18 22:33:15 +0600

    minor tweaks to content

--- src/components/WhyHireMe.jsx
+++ src/components/WhyHireMe.jsx
@@ -17,7 +17,7 @@ const WhyHireMe = () => {
           problem-solving skills and attention to detail ensure that I deliver
           clean, efficient code for responsive and user-friendly applications.
           I’m highly adaptable, continuously learning, and passionate about
-          keeping up with industry trends. With a proven ability to collaborate
+          keeping up with industry trends. With the ability to collaborate
           within teams and meet deadlines, I’m dedicated to delivering
           high-quality work and contributing to the success of any project.
         </p>
